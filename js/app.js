1b2e27ecb51cef3e953ef0afb52c00788cf29c83
pipe473 <bedoyafelipe743@gmail.com>
2022-02-19 18:55:57 +0100

    Añadiendo efecto en el menu de navegacion cuando se haga scroll

---
+++ js/app.js
@@ -0,0 +1,82 @@
+$(document).ready(function () {
+    // alert("Hello world!");
+
+    // MENU DE NAVEGACION
+    var active = true;
+
+    $(".btn-menu").on("click", function () {
+        
+
+        // INTERCALAR ICONOS
+        $(".btn-menu i").toggleClass("fa-times");
+
+
+        // ANIMACION MENU
+        if (active) {
+            $(".list-container").animate({
+                left: "0px"
+            }, 500);
+
+            active = false;
+        }else{
+            active = true;
+
+            $(".list-container").animate({
+                left: "-100%"
+            }, 500);
+
+            active = true;
+        }
+    });
+
+    // INTERCALAR CLASE 'ACTIVE' ENLACES DE NAVEGACIÓN
+    var enlaces = document.querySelectorAll(".lists li a");
+
+    enlaces.forEach((element) => {
+        element.addEventListener("click", (event) => {
+            enlaces.forEach((link) => {
+                link.classList.remove("active");
+            });
+            event.target.classList.add("active");
+        });
+    });
+
+    // SCROLL EFECT
+
+    var prevScrollPos = window.pageYOffset;
+
+    window.onscroll = () =>{
+
+        // OCULTAR Y MOSTRAR MENU AL HACER SCROLL
+        var currentScrollPos = window.pageYOffset;
+
+        if (prevScrollPos > currentScrollPos ) {
+            $(".menu").css("top", "0px");
+            $(".menu").css("transition", "0.5s");
+
+        }else{
+            $(".menu").css("top", "-60px");
+            $(".menu").css("transition", "0.5s");
+        }
+        prevScrollPos = currentScrollPos;
+
+        // CONDICIONES
+        var posicion = window.pageYOffset;
+
+        if (posicion <= 600) {
+            // OCULTAR ESTILOS MENU SCROLL
+            $(".menu").css("boderBottom", "none");
+
+            // OCULTAR IR ARRIBA
+            $(".go-top").css("right", "-100%");
+        }else{
+            // MOSTRAR ESTILOS MENU SCROLL
+            $(".menu").css("boderBottom", "3px solid #ff2e63");
+
+             // MOSTRAR IR ARRIBA
+             $(".go-top").css("right", '0');
+             $(".go-top").css("transition", "500ms");
+        }
+    }
+
+});
